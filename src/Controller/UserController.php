954da2de9b59bf2741dea465e747a8453b826378
Valentin Pradel <val.pxl.vp@gmail.com>
2023-07-25 15:24:31 +0200

    User Route

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
+#[Route('/user')]
 class UserController extends AbstractController
 {
     #[Route('/profile', name: 'user_profile')]
