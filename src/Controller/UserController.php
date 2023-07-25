a37b57b6adc132f094e7ae8c5a4cd6756f34a2d2
Guerric Cochelin <cochelin.guerric@hotmail.com>
2023-07-25 11:58:39 +0200

    affichage du profil

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,12 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class UserController extends AbstractController
 {
     #[Route('/user', name: 'user_profile')]
     public function profile(): Response
     {
+
+
         return $this->render('user/profile.html.twig', [
+
 
         ]);
     }
