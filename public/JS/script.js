cf4edee8585db86a453337d3211f6ca2949f28cd
Nabil SI-SMAIL <nabilsys8@gmail.com>
2023-07-27 00:22:19 +0200

    wallah cette fois ci le loader il marche et ça tue

--- public/JS/script.js
+++ public/JS/script.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", () => {
+/*window.addEventListener("load", () => {
     const loader = document.querySelector(".loader");
 
     loader.classList.add("slide-up-animation");
@@ -6,4 +6,9 @@ window.addEventListener("load", () => {
     loader.addEventListener("transitionend", () =>{
         document.body.removeChild(loader);
     })
-})
+})*/
+const preloader = document.querySelector("[data-preload]");
+window.addEventListener("load", function(){
+    preloader.classList.add("loaded");
+    document.body.classList.add("loaded");
+});
