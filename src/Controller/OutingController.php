91db5a9c4e4f84f6b7afd54c16c50caae2192dfd
Nabil SI-SMAIL <nabilsys8@gmail.com>
2023-07-26 02:10:18 +0200

    list query + refactor outings

--- src/Controller/OutingController.php
+++ src/Controller/OutingController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Outing;
+use App\Repository\OutingRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Date;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 class OutingController extends AbstractController
 {
-    #[Route('/outing', name: 'app_outing')]
-    public function index(): Response
+    #[Route('/list-sorties', name: 'app_outing')]
+    public function list(OutingRepository $sortieRepository): Response
     {
+
+        $outings = $sortieRepository->findBy([],[ 'dateHeureDebut' => 'DESC']);
+
         return $this->render('outing/outing.html.twig', [
-            'controller_name' => 'OutingController',
+            "outings" => $outings
         ]);
     }
 }
