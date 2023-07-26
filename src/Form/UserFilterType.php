002db60ed262b05c1bb83d96cb74ecb3576cbfe8
Valentin Pradel <val.pxl.vp@gmail.com>
2023-07-26 19:00:57 +0200

    Add filters on User admin list

--- src/Form/UserFilterType.php
+++ src/Form/UserFilterType.php
@@ -13,7 +13,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserFilterType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('campus', EntityType::class, [
@@ -40,7 +40,7 @@ class UserFilterType extends AbstractType
         ;
     }
 
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'method' => 'GET',
